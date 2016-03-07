a4ede4136fc56f975da73c5b5f48f508053bdf64
Dan Weiner <daweiner16@gmail.com>
2016-03-07 13:25:04 -0500

    Add Edit action

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,53 @@
+# require 'rails_helper'
+
+# RSpec.feature "Users can edit existing projects" do
+
+#   before do
+#     FactoryGirl.create(:project, name: "Sublime Text 3")
+
+#     visit "/"
+#     click_link "Sublime Text 3"
+#     click_link "Edit Project"
+#   end
+#   scenario "with valid attributes" do
+    
+#     fill_in "Name", with: "Sublime Text 4 beta"
+#     click_button "Update Project"
+
+#     expect(page).to have_content "Project has been updated."
+#     expect(page).to have_content "Sublime Text 4 beta."
+#   end
+
+#   scenario "when providing invalid attributes" do
+#     fill_in "Name", with: ""
+#     click_button "Update Project"
+
+#     expect(page).to have_content "Project has not been updated."
+#   end
+# end
+
+require 'spec_helper'
+
+feature "Editing Projects" do
+  before do
+    FactoryGirl.create(:project, name: "TextMate 2")
+
+    visit "/"
+    click_link "TextMate 2"
+    click_link "Edit Project"
+  end
+
+  scenario "Updating a project" do
+    fill_in "Name", with: "TextMate 2 beta"
+    click_button "Update Project"
+
+    expect(page).to have_content("Project has been updated.")
+  end
+
+  scenario "Updating a project with invalid attributes is bad" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+
+    expect(page).to have_content("Project has not been updated.")
+  end
+end
