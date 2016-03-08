89b11083e8670209af4d50c0badc36d651aa902e
Dan Weiner <daweiner16@gmail.com>
2016-03-08 16:42:18 -0500

    Implement creating tickets for a project

---
+++ spec/features/creating_tickets_spec.rb
@@ -0,0 +1,86 @@
+require "rails_helper"
+
+RSpec.feature "Users can create new tickets" do
+  before do
+    project = FactoryGirl.create(:project, name: "Internet Explorer")
+
+    visit project_path(project)
+    click_link "New Ticket"
+  end
+
+  scenario "with valid attributes" do
+    fill_in "Name", with: "Non-standards compliance"
+    fill_in "Description", with: "My pages are ugly!"
+    click_button "Create Ticket"
+
+    expect(page).to have_content "Ticket has been created."
+  end
+
+  scenario "when providing invalid attributes" do
+    click_button "Create Ticket"
+
+    expect(page).to have_content "Ticket has not been created."
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Description can't be blank"
+  end
+
+  scenario "with an invalid description" do
+    fill_in "Name", with: "Non-standards compliance"
+    fill_in "Description", with: "It sucks"
+    click_button "Create Ticket"
+
+    expect(page).to have_content "Ticket has not been created."
+    expect(page).to have_content "Description is too short"
+  end
+end
+
+# require 'rails_helper'
+
+# RSpec.feature "Creating Tickets" do
+#   before do
+#     project = FactoryGirl.create(:project)
+#     # user = FactoryGirl.create(:user)
+#     visit project_path(project)
+#     # visit '/'
+#     # click_link project.name
+#     click_link "New Ticket"
+#     message = "You need to sign in or sign up before continuing."
+#     expect(page).to have_content(message)
+
+#     fill_in "Name", with: user.name
+#     fill_in "Password", with: user.password
+#     click_button "Sign in"
+
+#     click_link project.name
+#     click_link "New Ticket"
+#   end
+
+#   scenario "Creating a ticket" do
+#     fill_in "Title", with: "Non-standards compliance"
+#     fill_in "Description", with: "My pages are ugly!"
+#     click_button "Create Ticket"
+
+#     expect(page).to have_content("Ticket has been created.")
+
+#     within "#ticket #author" do
+#       expect(page).to have_content("Created by example@example.com")
+#     end
+#   end
+
+#   scenario "Creating a ticket without valid attributes fails" do
+#     click_button "Create Ticket"
+
+#     expect(page).to have_content("Ticket has not been created.")
+#     expect(page).to have_content("Title can't be blank")
+#     expect(page).to have_content("Description can't be blank")
+#   end
+
+#   scenario "Description must be longer than 10 characters" do
+#     fill_in "Title", with: "Non-standards compliance"
+#     fill_in "Description", with: "it sucks"
+#     click_button "Create Ticket"
+
+#     expect(page).to have_content("Ticket has not been created.")
+#     expect(page).to have_content("Description is too short")
+#   end
+# end
