da453d3a5cdf6142c29c59e87963c27fb18a545f
Dan Weiner <daweiner16@gmail.com>
2016-03-09 11:55:14 -0500

    Added admin flag to User model, and seeded the first admin user

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,63 +1,65 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature "Users can create new projects" do
+RSpec.feature "Users can create new projects" do
 
-#   before do
-#     visit "/"
-
-#     click_link "New Project"
-#   end
-
-#   scenario "with valid attributes" do
-#     fill_in "Name", with: "Sublime Text 3"
-#     fill_in "Description", with: "A text editor for everyone"
-#     click_button "Create Project"
-
-#     expect(page).to have_content "Project has been created"
-
-#     project = Project.find_by(name: "Sublime Text 3")
-#     expect(page.current_url).to eq project_url(project)
-
-#     title = "Sublime Text 3 - Projects - Ticketee"
-#     expect(page).to have_title title
-#   end
-
-#   scenario "when providing invalid attributes" do
-#     click_button "Create Project"
-
-#     expect(page).to have_content "Project has not been created."
-#     expect(page).to have_content "Name can't be blank."
-#   end
-# end
-
-require 'spec_helper'
-
-feature 'Creating Projects' do
   before do
-    visit '/'
+    login_as(FactoryGirl.create(:user, :admin))
+    visit "/"
 
-    click_link 'New Project'
+    click_link "New Project"
   end
 
-  scenario "can create a project" do
-    fill_in 'Name', with: 'TextMate 2'
-    fill_in 'Description', with: 'A text-editor for OS X'
-    click_button 'Create Project'
+  scenario "with valid attributes" do
+    fill_in "Name", with: "Sublime Text 3"
+    fill_in "Description", with: "A text editor for everyone"
+    click_button "Create Project"
 
-    expect(page).to have_content('Project has been created.')
+    expect(page).to have_content "Project has been created"
 
-    project = Project.where(name: "TextMate 2").first
+    project = Project.find_by(name: "Sublime Text 3")
+    expect(page.current_url).to eq project_url(project)
 
-    expect(page.current_url).to eql(project_url(project))
-
-    title = "TextMate 2 - Projects - Ticketee"
-    expect(page).to have_title(title)
+    title = "Sublime Text 3 - Projects - Ticketee"
+    expect(page).to have_title title
   end
 
-  scenario "can not create a project without a name" do
-    click_button 'Create Project'
+  scenario "when providing invalid attributes" do
+    click_button "Create Project"
 
-    expect(page).to have_content("Project has not been created.")
-    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content "Project has not been created."
+    expect(page).to have_content "Name can't be blank"
   end
 end
+
+# require 'spec_helper'
+
+# feature 'Creating Projects' do
+#   before do
+#     login_as(FactoryGirl.create(:user, :admin))
+#     visit '/'
+
+#     click_link 'New Project'
+#   end
+
+#   scenario "can create a project" do
+#     fill_in 'Name', with: 'TextMate 2'
+#     fill_in 'Description', with: 'A text-editor for OS X'
+#     click_button 'Create Project'
+
+#     expect(page).to have_content('Project has been created.')
+
+#     project = Project.where(name: "TextMate 2").first
+
+#     expect(page.current_url).to eql(project_url(project))
+
+#     title = "TextMate 2 - Projects - Ticketee"
+#     expect(page).to have_title(title)
+#   end
+
+#   scenario "can not create a project without a name" do
+#     click_button 'Create Project'
+
+#     expect(page).to have_content("Project has not been created.")
+#     expect(page).to have_content("Name can't be blank")
+#   end
+# end
--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -1,22 +1,22 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.feature "Users can view projects" do 
-#   scenario "with project details" do
-#     project = FactoryGirl.create(:project, name: "Sublime Text 3")
+RSpec.feature "Users can view projects" do 
+  scenario "with project details" do
+    project = FactoryGirl.create(:project, name: "Sublime Text 3")
 
-#     visit "/"
-#     click_link "Sublime Text 3"
-#     expect(page.current_url).to eq project_url(project)
-#   end
-# end
-
-require 'spec_helper'
-
-feature "Viewing projects" do
-  scenario "Listing all projects" do
-    project = FactoryGirl.create(:project, name: "TextMate 2")
-    visit '/'
-    click_link 'TextMate 2'
-    expect(page.current_url).to eql(project_url(project))
+    visit "/"
+    click_link "Sublime Text 3"
+    expect(page.current_url).to eq project_url(project)
   end
 end
+
+# require 'spec_helper'
+
+# feature "Viewing projects" do
+#   scenario "Listing all projects" do
+#     project = FactoryGirl.create(:project, name: "TextMate 2")
+#     visit '/'
+#     click_link 'TextMate 2'
+#     expect(page.current_url).to eql(project_url(project))
+#   end
+# end
